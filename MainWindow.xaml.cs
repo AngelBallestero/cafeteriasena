b36e16a796947a781e70cf6b643e1c337cf9a0ef
Luis Angel <luisgilballestero@gmail.com>
2024-06-17 14:28:40 -0500

    terminar en casa

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using cafeteriaSena.models;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cafeteriaSena
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (GestioncafeteriaContext db = new GestioncafeteriaContext())
+            {
+                string usuario = txtusuario.Text;
+                int contraseña = int.Parse(txtcontraseña.Text);
+ 
+            }
+           
+        }
+    }
+}
